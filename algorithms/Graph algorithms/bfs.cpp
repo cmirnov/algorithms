15b1c360c48f1a4658a685ef94fdcf961c112351
cmirnov <cmirnov239kirill@gmail.com>
2017-02-04 20:14:15 +0300

    add bfs

---
+++ algorithms/Graph algorithms/bfs.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector < vector <int> > neib; //adjacency list 
+int n; //number of vertexs
+vector <bool> color;
+
+void bfs() {
+	queue <int> q;
+  for (int i = 0; i < n; ++i) {
+		if (!color[i]) {
+      q.push(i);
+			while (!q.empty()) {
+				int u = q.front();
+				color[u] = true;
+				q.pop();
+				for (int j = 0; j < neib[u].size(); ++j) {
+					int to = neib[u][j];
+					if (!color[to]) {
+						q.push(to);
+					}
+				}
+			}
+		}
+  }
+	return;
+}
+
+int main() {
+	bfs();
+	return 0;
+}
