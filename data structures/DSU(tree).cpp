27a365ffd0c4eec9870599a2910c71286aae4153
cmirnov <cmirnov239kirill@gmail.com>
2017-02-10 11:36:49 +0300

    DSU(tree)

---
+++ data structures/DSU(tree).cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class DSU {
+  vector <int> parent;
+public:
+
+  DSU (int n) {
+    parent.resize(n);
+    for (int i = 0; i < n; ++i) {
+      parent[i] = i;
+    }
+  }
+
+  int get (int u) {
+    return parent[u] == u ? u : get(parent[u]);
+  }
+
+  void join (int u, int v) {
+    parent[get(u)] = get(v);
+  }
+
+  bool is (int u, int v) {
+    return get(u) == get(v);
+  }
+};
+
+int main() {
+
+  return 0;
+}
